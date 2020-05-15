d4fd36df3d00077ddb562b6aa5d0e696fb3b4949
Zoilo Granda <zgranda@gmail.com>
2020-05-15 16:37:31 -0400

    Step lines

--- src/components/Steps.vue
+++ src/components/Steps.vue
@@ -1,34 +1,65 @@
 <template>
-  <div class="container flex">
-    <div class="step">
-      <img src="../assets/check.svg" class="icon" alt="checkmark" />
-      <p class="gray-text">Draft</p>
-      <p class="gray-text font-size-small">Created on</p>
-      <p class="black-text font-size-small bold-text">01/31/2020</p>
+  <div class="">
+    <div class="flow-container flex">
+      <div class="step-1">
+        <img src="../assets/check.svg" class="icon" alt="checkmark" />
+      </div>
+      <div class="step-5">
+        <div class="line"></div>
+      </div>
+      <div class="step-1">
+        <img src="../assets/check.svg" class="icon" alt="checkmark" />
+      </div>
+      <div class="step-5">
+        <div class="line"></div>
+      </div>
+      <div class="step-1">
+        <span class="dot"><p>3</p></span>
+      </div>
+      <div class="step-5">
+        <div class="line"></div>
+      </div>
+      <div class="step-1">
+        <span class="dot"><p>4</p></span>
+      </div>
+      <div class="step-5">
+        <div class="line"></div>
+      </div>
+      <div class="step-1">
+        <span class="dot"><p>5</p></span>
+      </div>
+      <div class="step-5">
+        <div class="line"></div>
+      </div>
+      <div class="step-1">
+        <span class="dot"><p>6</p></span>
+      </div>
     </div>
-    <div class="step">
-      <img src="../assets/check.svg" class="icon" alt="checkmark" />
-      <p class="gray-text">Pending Approval</p>
-      <p class="gray-text font-size-small">Approval sent</p>
-      <p class="black-text font-size-small bold-text">02/14/2020</p>
-    </div>
-    <div class="step">
-      <span class="dot"></span>
-      <p class="gray-text">Approved</p>
-      <p class="gray-text font-size-small">Abandoned on</p>
-      <p class="black-text font-size-small bold-text">02/20/2020</p>
-    </div>
-    <div class="step">
-      <span class="dot"></span>
-      <p class="gray-text">Execute</p>
-    </div>
-    <div class="step">
-      <span class="dot"></span>
-      <p class="gray-text">Concluded</p>
-    </div>
-    <div class="step">
-      <span class="dot"></span>
-      <p class="gray-text">Closed</p>
+    <div class="description-container flex">
+      <div class="description-step">
+        <p class="gray-text">Draft</p>
+        <p class="gray-text font-size-small">Created on</p>
+        <p class="black-text font-size-small bold-text">01/31/2020</p>
+      </div>
+      <div class="description-step">
+        <p class="gray-text">Pending Approval</p>
+        <p class="gray-text font-size-small">Approval sent</p>
+        <p class="black-text font-size-small bold-text">02/14/2020</p>
+      </div>
+      <div class="description-step">
+        <p class="gray-text">Approved</p>
+        <p class="gray-text font-size-small">Abandoned on</p>
+        <p class="black-text font-size-small bold-text">02/20/2020</p>
+      </div>
+      <div class="description-step">
+        <p class="gray-text">Execute</p>
+      </div>
+      <div class="description-step">
+        <p class="gray-text">Concluded</p>
+      </div>
+      <div class="description-step">
+        <p class="gray-text">Closed</p>
+      </div>
     </div>
   </div>
 </template>
@@ -53,22 +84,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.container {
- height: 130px;
-  .step {
+.flow-container {
+  height: 30px;
+  .step-1 {
     flex-grow: 1;
     height: 30px;
+    // width: 10vw;
     .icon {
-      height: 25px;
-      width: 25px;
+      height: 20px;
+      width: 20px;
     }
     .dot {
-      height: 25px;
-      width: 25px;
+      height: 20px;
+      width: 20px;
       background-color: #bbb;
       border-radius: 50%;
       display: inline-block;
     }
+  }
+  .step-5{
+   flex-grow: 5;
+   .line {
+    width: 100%;
+    height: 0;
+    border: 1px solid #c4c4c4;
+    margin: 3px;
+    display: inline-block;
+   }
+  }
+}
+.description-container {
+  .description-step {
+    width: 10vw;
   }
 }
 </style>
